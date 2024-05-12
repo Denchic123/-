98a9b8f02d4ff44760d0a99a4cd463e48afa5985
Denchic123 <168342950+Denchic123@users.noreply.github.com>
2024-05-12 15:37:51 +0300

    Add files via upload

--- ping_pong.py
+++ ping_pong.py
@@ -16,8 +16,6 @@ class GameSprite(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
-        self.speed_x = player_speed
-        self.speed_y = player_speed
 
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
@@ -38,9 +36,31 @@ class Player(GameSprite):
             self.rect.y += self.speed
 
 class Ball(GameSprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, size_x, size_y):
+        super().__init__(player_image, player_x, player_y, player_speed, size_x, size_y)
+        self.speed_x = player_speed
+        self.speed_y = player_speed
+
     def update(self):
-        self.rect.y += self.speed_y
         self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+
+        if self.rect.y >= Win_HEIGHT:
+            self.speed_y *= -1
+
+        if self.rect.y <= 0:
+            self.speed_y *= -1
+
+        # if self.rect.x >= Win_WIDTH:
+        #     self.speed_x *= -1
+
+        # if self.rect.x <= 0:
+        #     self.rect.x *= -1
+
+    def collide_rect(self, player):
+        if self.rect.colliderect(player):
+            self.speed_x *= -1
+
 
 
 player1 = Player('racket.png', 5,Win_HEIGHT - 200, 10, 50, 150)
@@ -49,11 +69,12 @@ ball = Ball('tenis_ball.png',200, 200, 10, 50, 50)
 
 game = True
 finish = False
+win = None
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-    window.fill((250, 243, 177))
+    window.fill((250, 243, 177))   
     if not finish:
         player1.updateL()
         player1.reset()
@@ -61,5 +82,17 @@ while game:
         player2.reset()
         ball.update()
         ball.reset()
+        
+        ball.collide_rect(player1)
+        ball.collide_rect(player2)
+
+        if ball.rect.x < 50:
+            winner = win1
+            finish = True
+        if ball.rect.x > Win_WIDTH - 50:
+            winner = win2
+            finish = True
+    elif finish:
+        window.blit(winner, (50, 200))    
     display.update()
     clock.tick(FPS)
