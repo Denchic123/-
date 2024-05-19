bf5fcbd80d1fcf1bae2ea397bd7866729319a166
Denchic123 <168342950+Denchic123@users.noreply.github.com>
2024-05-19 14:31:28 +0300

    Add files via upload

--- ping_pong.py
+++ ping_pong.py
@@ -1,15 +1,12 @@
 from pygame import *
-"Размер окна"
-Win_HEIGHT = 500
-Win_WIDTH = 700
-window = display.set_mode((Win_WIDTH, Win_HEIGHT))
-display.set_caption('ping_pong')
-window.fill((250, 243, 177))
-FPS = 60
-clock = time.Clock()
 
+
+WIN_WIDTH = 700
+WIN_HEIGHT = 500
+FPS = 40
+bg = (250, 200, 147)
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, player_speed, size_x, size_y):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
         super().__init__()
         self.image = transform.scale(image.load(player_image), (size_x, size_y))
         self.speed = player_speed
@@ -20,79 +17,91 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-class Player(GameSprite):
-    def updateL(self):
-        keys = key.get_pressed()
-        if keys[K_UP] and self.rect.y >= 5:
-            self.rect.y -= self.speed
-        elif keys[K_DOWN] and self.rect.y <= Win_HEIGHT- 150:
-            self.rect.y += self.speed
-
-    def updateR(self):
+class Player1(GameSprite):
+    def update_l(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y >= 5:
             self.rect.y -= self.speed
-        elif keys[K_s] and self.rect.y <= Win_HEIGHT - 150:
+        elif keys[K_s] and self.rect.y <= WIN_HEIGHT - 155:
+            self.rect.y += self.speed
+    
+class Player2(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y >= 5:
+            self.rect.y -= self.speed
+        elif keys[K_DOWN] and self.rect.y <= WIN_HEIGHT - 155:
             self.rect.y += self.speed
 
 class Ball(GameSprite):
-    def __init__(self, player_image, player_x, player_y, player_speed, size_x, size_y):
-        super().__init__(player_image, player_x, player_y, player_speed, size_x, size_y)
-        self.speed_x = player_speed
-        self.speed_y = player_speed
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, speed_x, speed_y):
+        super().__init__(player_image, player_x, player_y, size_x, size_y,speed_x)
+        self.speed_x = speed_x
+        self.speed_y = speed_y
 
     def update(self):
         self.rect.x += self.speed_x
         self.rect.y += self.speed_y
 
-        if self.rect.y >= Win_HEIGHT:
+        if self.rect.y >= WIN_HEIGHT-50:
             self.speed_y *= -1
 
         if self.rect.y <= 0:
             self.speed_y *= -1
 
-        # if self.rect.x >= Win_WIDTH:
-        #     self.speed_x *= -1
-
-        # if self.rect.x <= 0:
-        #     self.rect.x *= -1
-
     def collide_rect(self, player):
         if self.rect.colliderect(player):
             self.speed_x *= -1
 
+window = display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+display.set_caption('PingPong')
+
+clock = time.Clock()
 
 
-player1 = Player('racket.png', 5,Win_HEIGHT - 200, 10, 50, 150)
-player2 = Player('racket.png', 645,Win_HEIGHT - 200, 10, 50, 150)
-ball = Ball('tenis_ball.png',200, 200, 10, 50, 50)
+player1 = Player1('racket.png', 50, WIN_HEIGHT - 280, 50, 150, 4)
+
+player2 = Player2('racket.png', WIN_WIDTH - 100, WIN_HEIGHT - 280, 50, 150, 4)
+
+ball = Ball('tenis_ball.png', 325, WIN_HEIGHT - 280, 50, 50, 7, 7)
 
 game = True
 finish = False
-win = None
+
+font.init()
+font_win = font.SysFont('Arial', 70)
+win_r = font_win.render('Выйграл 2 игрок!', True, (0, 0, 255))
+win_l = font_win.render('Выйграл 1 игрок!', True, (255, 0, 0))
+winner = None
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-    window.fill((250, 243, 177))   
     if not finish:
-        player1.updateL()
+        window.fill(bg)
+
         player1.reset()
-        player2.updateR()
+        player1.update_l()
         player2.reset()
-        ball.update()
+        player2.update_r()
         ball.reset()
-        
+        ball.update()
         ball.collide_rect(player1)
         ball.collide_rect(player2)
 
-        if ball.rect.x < 50:
-            winner = win1
+        if ball.rect.x < 0:
+            winner = win_r
             finish = True
-        if ball.rect.x > Win_WIDTH - 50:
-            winner = win2
+        
+        if ball.rect.x > WIN_WIDTH - 50:
+            winner = win_l
             finish = True
+
+        
+
     elif finish:
-        window.blit(winner, (50, 200))    
+        window.blit(winner, (75, 200))
+
     display.update()
     clock.tick(FPS)
